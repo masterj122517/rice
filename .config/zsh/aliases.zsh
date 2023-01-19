42b30bab246b1ef61ca85e22c4ff1dd35a77ae2c
MasterJ <masterj122517@163.com>
2023-01-19 21:45:35 +0800

    some shit

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,34 @@
+# window swallow 
+alias vlc='ws vlc'
+alias zathura='ws zathura'
+alias mpv='ws mpv'
+
+
+
+alias ll='exa'
+alias l='exa -al'
+alias la='exa -a'
+alias pacin='sudo pacman -S'
+alias gita='git add'
+alias gitc='git commit'
+alias ra='ranger'
+alias r='ranger'
+alias paclean='sudo pacman -Sc'
+alias pacu='sudo pacman -Rns'
+alias gitp='git push'
+alias c='clear'
+alias s='neofetch'
+alias cat='bat'
+alias za='ws zathura'
+alias pacman='sudo pacman'
+alias locate='plocate'
+alias setproxy="export ALL_PROXY=socks5://127.0.0.1:7890"
+alias unsetproxy="unset ALL_PROXY"
+alias pc="proxychains4"
+alias tcn="trans zh:en"
+alias ten="trans en:zh"
+alias nb='newsboat'
+alias pcnb='proxychains4 newsboat'
+alias lg='lazygit'
+alias cbg='cd ~/.local/blog/MasterJ/source/_posts/'
+alias vi='nvim'
