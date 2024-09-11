8fbc1ac6108558622deee76400885d802ddab06b
masterj <masterj122517@163.com>
2024-09-11 19:59:14 +0800

    change ranger errror

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -6,15 +6,17 @@ from ranger.core.loader import CommandLoader
 class paste_as_root(Command):
     def execute(self):
         if self.fm.do_cut:
-            self.fm.execute_console('shell sudo mv %c .')
+            self.fm.execute_console("shell sudo mv %c .")
         else:
-            self.fm.execute_console('shell sudo cp -r %c .')
+            self.fm.execute_console("shell sudo cp -r %c .")
+
 
 class setbg(Command):
     def execute(self):
-        tfile=self.fm.thisfile
-        wal=tfile.path
-        self.fm.execute_console('shell setbg'+" "+wal) 
+        tfile = self.fm.thisfile
+        wal = tfile.path
+        self.fm.execute_console("shell setbg" + " " + wal)
+
 
 class toggleVCS(Command):
     def execute(self):
@@ -37,20 +39,21 @@ class mkcd(Command):
         if not lexists(dirname):
             makedirs(dirname)
 
-            match = re.search('^/|^~[^/]*/', dirname)
+            match = re.search("^/|^~[^/]*/", dirname)
             if match:
                 self.fm.cd(match.group(0))
-                dirname = dirname[match.end(0):]
+                dirname = dirname[match.end(0) :]
 
-            for m in re.finditer('[^/]+', dirname):
+            for m in re.finditer("[^/]+", dirname):
                 s = m.group(0)
-                if s == '..' or (s.startswith('.')
-                                 and not self.fm.settings['show_hidden']):
+                if s == ".." or (
+                    s.startswith(".") and not self.fm.settings["show_hidden"]
+                ):
                     self.fm.cd(s)
                 else:
                     # We force ranger to load content before calling `scout`.
                     self.fm.thisdir.load_content(schedule=False)
-                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+                    self.fm.execute_console("scout -ae ^{}$".format(s))
         else:
             self.fm.notify("file/directory exists!", bad=True)
 
@@ -66,22 +69,27 @@ class fzf_select(Command):
     def execute(self):
         import subprocess
         import os.path
+
         if self.quantifier:
             # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = (
+                "find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            )
 
         else:
             # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = (
+                "find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            )
 
-        fzf = self.fm.execute_command(command,
-                                      universal_newlines=True,
-                                      stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command(
+            command, universal_newlines=True, stdout=subprocess.PIPE
+        )
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            fzf_file = os.path.abspath(stdout.rstrip("\n"))
             if os.path.isdir(fzf_file):
                 self.fm.cd(fzf_file)
             else:
@@ -90,7 +98,7 @@ class fzf_select(Command):
 
 class compress(Command):
     def execute(self):
-        """ Compress marked files to current directory """
+        """Compress marked files to current directory"""
         cwd = self.fm.thisdir
         marked_files = cwd.get_selection()
 
@@ -106,25 +114,30 @@ class compress(Command):
         au_flags = parts[1:]
 
         descr = "compressing files in: " + os.path.basename(parts[1])
-        obj = CommandLoader(args=['apack'] + au_flags +
-                            [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
+        obj = CommandLoader(
+            args=["apack"]
+            + au_flags
+            + [os.path.relpath(f.path, cwd.path) for f in marked_files],
+            descr=descr,
+            read=True,
+        )
 
-        obj.signal_bind('after', refresh)
+        obj.signal_bind("after", refresh)
         self.fm.loader.add(obj)
 
     def tab(self, tabnum):
-        """ Complete with current folder name """
+        """Complete with current folder name"""
 
-        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        extension = [".zip", ".tar.gz", ".rar", ".7z"]
         return [
-            'compress ' + os.path.basename(self.fm.thisdir.path) + ext
+            "compress " + os.path.basename(self.fm.thisdir.path) + ext
             for ext in extension
         ]
 
 
 class extracthere(Command):
     def execute(self):
-        """ Extract copied files to current directory """
+        """Extract copied files to current directory"""
         copied_files = tuple(self.fm.copy_buffer)
 
         if not copied_files:
@@ -137,19 +150,21 @@ class extracthere(Command):
         one_file = copied_files[0]
         cwd = self.fm.thisdir
         original_path = cwd.path
-        au_flags = ['-X', cwd.path]
+        au_flags = ["-X", cwd.path]
         au_flags += self.line.split()[1:]
-        au_flags += ['-e']
+        au_flags += ["-e"]
 
         self.fm.copy_buffer.clear()
         self.fm.cut_buffer = False
         if len(copied_files) == 1:
             descr = "extracting: " + os.path.basename(one_file.path)
         else:
-            descr = "extracting files from: " + os.path.basename(
-                one_file.dirname)
-        obj = CommandLoader(args=['aunpack'] + au_flags
-                            + [f.path for f in copied_files], descr=descr, read=True)
+            descr = "extracting files from: " + os.path.basename(one_file.dirname)
+        obj = CommandLoader(
+            args=["aunpack"] + au_flags + [f.path for f in copied_files],
+            descr=descr,
+            read=True,
+        )
 
-        obj.signal_bind('after', refresh)
+        obj.signal_bind("after", refresh)
         self.fm.loader.add(obj)
